68ee1d1e6d48ed02ab8efcadb241f80547a7f6ba
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-05-02 21:56:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.link("esposée", "https://docs.google.com/presentation/d/1zWbOhAsGVLhCHIdc6nJWB-GG5Udkuxx9cE2rlSJ_FT0/edit#slide=id.g2d1d413eaaa_0_1")
+st.markdown("https://docs.google.com/presentation/d/1zWbOhAsGVLhCHIdc6nJWB-GG5Udkuxx9cE2rlSJ_FT0/edit#slide=id.g2d1d413eaaa_0_1")
